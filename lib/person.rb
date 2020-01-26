2de67c9ac1d08f2448ded955cb262fd01d4ac012
Damely Tineo <damely.tineo07@gmail.com>
2020-01-26 04:39:34 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -2,7 +2,7 @@ class Person
   
   attr_accessor :name, :birthday 
   
-  def initilize(name:, birthday:)
+  def initialize(name:, birthday:)
     
     attributes.each{|key, value| self.send(("#{key}="), value)}
   end
